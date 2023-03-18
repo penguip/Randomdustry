a875eca31d931e49b351643f63e21c36187de3ae
penguip <99997237+penguip@users.noreply.github.com>
2023-03-18 18:16:56 -0500

    Update tmain.js

--- scripts/tmain.js
+++ scripts/tmain.js
@@ -6,7 +6,9 @@ Blocks.siliconSmelter.outputItems = ItemStack.with(Items.surgeAlloy, 1, Items.si
 Blocks.siliconSmelter.consumeItem(Items.coal, 1)
 Blocks.siliconCrucible.consumeItem(Items.silicon, 1)
 Blocks.siliconCrucible.outputItems = ItemStack.with(Items.pyratite, 3, Items.surgeAlloy, 8, Items.phaseFabric, 16, Items.blastCompound, 1)
-Blocks.foreshadow.type = powerTurret
 Blocks.foreshadow.ammoTypes.remove(Items.surgeAlloy)
+Blocks.yellowStoneWall.itemDrop = Items.oxide
+Blocks.regolithWall.itemDrop = Items.oxide
+Blocks.redStoneWall.itemDrop = Items.silicon
 Blocks.foreshadow.reload = 0
 });
