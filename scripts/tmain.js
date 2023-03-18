cb87e4b35de6dcfab2013d11a616965da19287ab
penguip <99997237+penguip@users.noreply.github.com>
2023-03-18 06:16:53 -0500

    Update tmain.js

--- scripts/tmain.js
+++ scripts/tmain.js
@@ -2,5 +2,5 @@
 
 
 Events.on(ClientLoadEvent, b  => {
-Blocks.siliconSmelter.outputItems(Items.surgeAlloy, 1, Items.silicon, 1, Items.phaseFabric, 2)
+Blocks.siliconSmelter.outputItems = ItemStack.with(Items.surgeAlloy, 1, Items.silicon, 1, Items.phaseFabric, 2)
 });
